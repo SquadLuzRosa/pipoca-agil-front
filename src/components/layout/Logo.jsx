228eb5e12efb9d59a5cbef504f26ac2c2bdf0a9a
thiagoferreirapy <tf938383@gmail.com>
2023-12-21 10:03:20 -0300

    update: Logo size update

---
+++ src/components/layout/Logo.jsx
@@ -0,0 +1,12 @@
+import style from './Logo.module.css'
+
+function Logo() {
+    return (
+      <>
+        <img className={style.img_logo} src="/logo.jpg" />
+      </>
+    )
+  }
+  
+  export default Logo
+  
